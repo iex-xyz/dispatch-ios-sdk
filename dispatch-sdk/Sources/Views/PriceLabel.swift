9a8075b378eb3eeeaf11bb7000800b4d28f08f4c
Stephen Silber <stephensilber@gmail.com>
2024-04-17 22:29:39 -0400

    Adds price label component

--- dispatch-sdk/Sources/Views/PriceLabel.swift
+++ dispatch-sdk/Sources/Views/PriceLabel.swift
@@ -1,8 +1,39 @@
-//
-//  PriceLabel.swift
-//  dispatch-sdk
-//
-//  Created by Stephen Silber on 4/17/24.
-//
+import SwiftUI
 
-import Foundation
+struct PriceLabel: View {
+    let basePrice: String
+    let currentPrice: String?
+    let theme: Theme
+    
+    var body: some View {
+        HStack {
+            if let currentPrice {
+                Text(currentPrice)
+                    .foregroundStyle(Color.dispatchGreen)
+                Text(basePrice)
+                    .strikethrough(true, color: Color.dispatchLightGray)
+                    .foregroundStyle(Color.dispatchLightGray)
+            } else {
+                Text(basePrice)
+                    .foregroundStyle(.primary)
+
+            }
+        }
+        .font(.headline)
+    }
+}
+
+#Preview {
+    HStack(spacing: 32) {
+        VStack(alignment: .leading) {
+            PriceLabel(basePrice: "$76.97", currentPrice: nil, theme: .default)
+            PriceLabel(basePrice: "$85.00", currentPrice: "$76.97", theme: .default)
+        }
+        VStack(alignment: .leading) {
+            PriceLabel(basePrice: "$76.97", currentPrice: nil, theme: .mock(mode: .dark))
+            PriceLabel(basePrice: "$85.00", currentPrice: "$76.97", theme: .mock(mode: .dark))
+        }
+    }
+    .frame(width: 300, height: 200)
+    .background(Color.dispatchDarkGray)
+}
