3bc92cbdabfb2ae256f76f0ed145186b2b19a338
Stephen Silber <stephensilber@gmail.com>
2024-05-02 00:05:32 -0400

    Updates demo status string

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -7,14 +7,23 @@ struct ContentView: View {
     let buildInfo = """
 • Apple Pay is setup but needs proper mapping to Dispatch API models (line items, shipping methods, etc)
 
+• Custom navigation title merchant tag isn't resizing width or marquee scrolling properly
+
 • Navigation hasn't been updated to match latest Figma. Still some questions/research around feasibility going sheet -> fullscreen
 
-• Checkout order preview isn't wired to handle going in and editing values yet
+• Checkout preview needs some testing with multiple attribute variant
 
-• Billing info update was throwing an error on the API side. Needs investigation
+• Some loading states on buttons aren't being set properly
 
-• Shipping method text needs to match web formatting
+• SVG image support is not sizing properly
 
+• HTML needs more testing with various tags and test cases. Staging presets have weird characters and missing HTML tags
+
+• Confetti success needs to be polished
+
+• PhoneNumberTextfield needs more testing to verify full global support works as intended
+
+• Rotation indicator is not implemented yet
 
 """
     var body: some View {
