2ba6df35e65d6fe261227d6a01cdd6f42eea1c73
Stephen Silber <stephensilber@gmail.com>
2024-05-20 11:10:10 -0400

    Bumps touch area for close button throughout

--- Sources/DispatchSDK/Sources/UI/CloseButton.swift
+++ Sources/DispatchSDK/Sources/UI/CloseButton.swift
@@ -2,32 +2,36 @@ import SwiftUI
 
 @available(iOS 15.0, *)
 struct CloseButton: View {
+    // Assuming `theme` is correctly implemented and accessible here.
     @Preference(\.theme) var theme
+
     let handler: () -> Void
     
+    // Ensure the touchable area covers the whole 44x44 area.
     var body: some View {
-        Button(action: {
-            handler()
-        }) {
+        Button(action: handler) {
             Image(systemName: "xmark")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 10, height: 10)
+                .padding()
         }
-        .foregroundStyle(.primary)
         .background(
             Circle()
                 .fill(Colors.secondaryBackgroundColor)
                 .frame(width: 30, height: 30)
         )
-        .frame(width: 40, height: 40)
-        .colorScheme(theme.colorScheme)
+        .frame(width: 44, height: 44)
+        .foregroundColor(.primary)
+        .clipShape(Circle())
     }
 }
 
 @available(iOS 15.0, *)
-#Preview {
-    CloseButton {
-        //
+struct CloseButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CloseButton {
+            // Action for the button
+        }
     }
 }
